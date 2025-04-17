e5b0812edf85233d55cf352de09115924b2aec00
Ebenezer Ansere <ebenezer.ansere@amalitech.com>
2025-04-17 09:35:03 +0000

    change script with upload skill

--- cypress/PageObject/delete_skill_csvfile.js
+++ cypress/PageObject/delete_skill_csvfile.js
@@ -4,9 +4,10 @@ class DeleteSkillCSVFile{
         uploadBulkSkillButton: () => cy.get('button[type="button"]'),
         downloadSampleCSV: () => cy.contains("Download Sample CSV"),
         uploadcsvfile: () => cy.get("input[type='file']"),
-        deleteskillicon: () => cy.get("img[alt='Delete File']")
-
-    }
+        deleteskillicon: () => cy.get("img[alt='Delete File']"),
+        clickonCancel: () => cy.contains("Cancel"),
+        confirmDelete: () => cy.get("button[type='button']")
+    }   
 
     deleteskillicon(){
         this.elements.uploadBulkSkillButton().eq(0).click({force:true})
@@ -16,8 +17,12 @@ class DeleteSkillCSVFile{
         this.elements.deleteskillicon().then(function(mydeletecsvfile){
             cy.wrap(mydeletecsvfile).click({force:true})
         })
+        this.elements.clickonCancel().click({force:true})
+        this.elements.confirmDelete().eq(2).then(function(myconfirmdelete){
+            cy.wrap(myconfirmdelete).click({force:true})
+        })
     }
-
+    
 
 }
 
